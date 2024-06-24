78bc734760138b8157d7dda8ee68c93a20fb3b92
Fix Store <support@fixstore.info>
2024-06-24 18:08:23 +0300

    Ready to add advanced print settings

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sys
-from config import IMAGE_PATH, PYTHON_MODULES_PATH, CONTOUR_IMAGE_FILE, IMAGE_PATH, PYTHON_MODULES_PATH, PNG_OUTPUT_FOLDER # Import the IMAGE_PATH and PYTHON_MODULES_PATH variables
+from config import IMAGE_PATH, PYTHON_MODULES_PATH, CONTOUR_IMAGE_FILE, IMAGE_PATH, PYTHON_MODULES_PATH, PNG_NO_ALPHA_FILE # Import the IMAGE_PATH and PYTHON_MODULES_PATH variables
 from contour import generate_contour
 from pdf_print import generate_pdf
 from trans_bg import replace_alpha_with_transparent
@@ -8,9 +8,11 @@ sys.path.append(PYTHON_MODULES_PATH)
 # CONTOUR Production
 contour_image = generate_contour(IMAGE_PATH) # Generate the contour image
 
-pdf1 = generate_pdf(IMAGE_PATH) # Generate the PDF file for IMAGE_PATH
-pdf2 = generate_pdf(CONTOUR_IMAGE_FILE) # Generate the PDF file for CONTOUR_IMAGE_FILE
-
-
-
 transparent_image = replace_alpha_with_transparent(IMAGE_PATH) # Generate the transparent image
+
+images_pdf = generate_pdf(IMAGE_PATH) # Generate the PDF file for uploaded image
+no_alpha_pdf = generate_pdf(PNG_NO_ALPHA_FILE) # Generate the PDF file for contours
+contours_pdf = generate_pdf(CONTOUR_IMAGE_FILE) # Generate the PDF file for contours
+
+
+
