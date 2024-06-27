ab7c7d009d0b41c0f2d524cdc6f0c752023340b1
Fix Store <support@fixstore.info>
2024-06-27 13:56:39 +0300

    Flask installed

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ from config import IMAGE_PATH, CONTOUR_IMAGE_FILE, IMAGE_PATH
 from contour import generate_contour
 from pdf_print import generate_pdf
 from trans_bg import replace_alpha_with_transparent
+from flask import request
+
+# Upload Image
+uploaded_image = request.files['image']
+image_path = '/path/to/save/image.jpg'
+uploaded_image.save(image_path)
+
+# Generate PDF for Uploaded Image
+uploaded_image_pdf = generate_pdf(image_path)
 
 # CONTOUR Production
 contour_image = generate_contour(IMAGE_PATH) # Generate the contour image
